ca2022ea992468391321eb2e74916c30ba1f5852
Onkar Suresh Choudhari <onkarschoudhari16@gmail.com>
2023-04-23 14:58:23 +0530

    new file add

---
+++ sptify/js/index.js
@@ -0,0 +1,10 @@
+document.getElementById("hi").addEventListener("click", loginpage);
+
+function loginpage() {
+  window.open('login.html', '_blank');
+}
+
+function myFunction() {
+  document.getElementById("myP").style.visibility = "visible";
+  
+}
